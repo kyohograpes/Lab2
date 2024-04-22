b05ee3af350042d6d3f8867e3ffd5a24da756029
kyohograpes <ganyuheng2006@gmail.com>
2024-04-22 15:59:58 +0800

    min max average added

--- lab2.py
+++ lab2.py
@@ -1,1 +1,38 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+def display_main_menu():
+    print("Enter some numbers separated by commas (e.g. 5, 67,32)")
+
+
+def getinput():
+    userinput=input("please enter the numbers")
+    strlist=userinput.split(",")
+    floatlist=[]
+    for x in strlist:
+        floatnum=float(x)
+        floatlist.append(floatnum)
+    return(floatlist)
+
+
+def avgtemp():
+    totallist=(getinput())
+    add=sum(totallist)
+    div=len(totallist)
+    avg=add/div
+    print(avg)
+    
+
+
+def minmax():
+    input=getinput()
+    mini=min(input)
+    maxi=max(input)
+    print("minimum is ",mini,"maximum is ",maxi)
+        
+    
+
+
+
+
+    
+avgtemp()
+minmax()
+
